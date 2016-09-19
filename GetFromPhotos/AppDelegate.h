7d8cdd2e0404606d2ff84e8b938e2f3df26778ca
Sheryl Han <SherylHan@example.com>
2016-09-19 16:16:24 +0800

    Initial Commit

---
+++ GetFromPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetFromPhotos
+//
+//  Created by 韩雪滢 on 9/19/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetFromPhotos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetFromPhotos
+//
+//  Created by 韩雪滢 on 9/19/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
