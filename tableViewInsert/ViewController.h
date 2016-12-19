8f008182319e2676d628f9ea1bbd0c4e92d9bdc0
Sheryl Han <SherylHan@example.com>
2016-12-19 19:51:56 +0800

    Initial Commit

---
+++ tableViewInsert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableViewInsert
+//
+//  Created by 韩雪滢 on 12/19/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableViewInsert/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tableViewInsert
+//
+//  Created by 韩雪滢 on 12/19/16.
+//  Copyright © 2016 韩雪滢. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
